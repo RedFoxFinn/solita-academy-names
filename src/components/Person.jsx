eb49460dc0f843f9257024de5209ff4682eda56e
RedFoxFinn <laborantti.antti@gmail.com>
2021-01-08 19:05:13 +0200

    perf(person): added multiline title to li-tag of <Person/>

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from '../tools/styles';
 
 const Person = (props) => {
-    return <li id={props.name + props.amount} style={styles.person()}>
+    return <li id={props.name + props.amount} style={styles.person()} title={`Name: ${props.name}\n Amount: ${props.amount}`} >
         <p style={styles.appName()}>{props.name}</p>
         <p style={styles.authorName()}>{props.amount}</p>
     </li>;
