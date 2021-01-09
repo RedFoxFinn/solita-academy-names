cb6d217cf62f14041c39f81669436cc571e410ba
RedFoxFinn <laborantti.antti@gmail.com>
2021-01-09 22:58:02 +0200

    test(app.test.js): added unit tests for App.js
    tests for rendering App & subcomponents Header & Viewer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { isCompositeComponent, isCompositeComponentWithType } from 'react-dom/test-utils';
+import uuid from 'uuid';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import Header from './components/Header';
+import Viewer from './components/Viewer';
+
+describe('App', () => {
+  const appId = uuid;
+  test('App renders', () => {
+    const app = render(<App id={appId}/>);
+    expect(app).toBeTruthy();
+    isCompositeComponentWithType(app, App);
+  });
+  test('Header renders', () => {
+    render(<App id={appId}/>);
+    const header = screen.getByTestId(`${appId}-header`);
+    expect(header).toBeTruthy();
+    isCompositeComponentWithType(header, Header);
+  });
+  test('Viewer renders', () => {
+    render(<App id={appId}/>);
+    const viewer = screen.getByTestId(`${appId}-viewer`);
+    expect(viewer).toBeTruthy();
+    isCompositeComponentWithType(viewer, Viewer);
+  });
 });
